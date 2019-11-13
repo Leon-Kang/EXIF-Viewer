fa6fbc2b235819b45da671ddb9e9b3ab73ba8473
Leon-Kang <coderleonkang@gmail.com>
2019-11-13 23:21:41 +0800

    Create meta data model

---
+++ EXIF Viewer/MetaData.swift
@@ -0,0 +1,21 @@
+//
+//  MetaData.swift
+//  EXIF Viewer
+//
+//  Created by LeonKang on 2019/11/13.
+//  Copyright © 2019 LeonKang. All rights reserved.
+//
+
+import Foundation
+
+enum MetaDataRootKey: String {
+    case exif = "{EXIF}"
+    case gps = "{GPS}"
+    case tiff = "{TIFF}"
+    case jfif = "{JFIF}"
+    case iptc = "{IPTC}"
+}
+
+public struct MetaData {
+    
+}
